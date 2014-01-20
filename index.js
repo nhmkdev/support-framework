c8606b1a3396838a7f19e046137863d283fd6eff
Tim Stair <nhmkdev@users.noreply.github.com>
2014-01-20 10:08:10 -0800

    Added xdk specific test files to help see the state of the android/ios builds without including the framework entirely. (conclusion: not good)

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 intel.xdk.context.include( 'lib/impact/impact.js' );
-intel.xdk.context.include( 'lib/game/main.js' );
+//intel.xdk.context.include( 'lib/game/main.js' );
+intel.xdk.context.include( 'lib/game/xdktest.js' );
 console.log('here!');
